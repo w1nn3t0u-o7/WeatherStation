7264f9301c5f484ac25b2251fee50f0133a63c94
w1nn3t0u <mik.ziel7890@gmail.com>
2024-12-19 02:21:11 +0100

    first commit

---
+++ components/bme280_i2c/include/bme280_i2c/bme280_i2c.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "bme280_common/bme280_common.hpp"
+#include "CPPI2C/cppi2c.hpp"
+
+namespace CPPBME280
+{
+    class BME280I2C : public BME280
+    {
+    private:
+        CPPI2C::I2c *i2c;
+        uint8_t _devAddress{};
+
+    protected:
+        esp_err_t writeByteData(const uint8_t reg, const uint8_t value);
+        int readByteData(const uint8_t reg);
+        int readWordData(const uint8_t reg);
+        esp_err_t readBlockData(const uint8_t reg, uint8_t *buf, const int length);
+
+    public:
+        void InitI2c(CPPI2C::I2c *i_i2c, const uint8_t dev_addr = 0x76);
+    }; // namespace CPPBME280
+} // namespace CPPBME280
---
+++ components/bme280_spi/include/bme280_spi/bme280_spi.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "bme280_common/bme280_common.hpp"
+#include "CPPSPI/cppspi.hpp"
+
+namespace CPPBME280
+{
+    constexpr static uint8_t MODE = 0;
+    constexpr static uint8_t ADDR_BITS = 7;
+    constexpr static uint8_t CMD_BITS = 1;
+
+    constexpr static uint8_t SPI_WRITE = 0;
+    constexpr static uint8_t SPI_READ = 1;
+
+    class BME280SPI : public BME280
+    {
+    private:
+        CPPSPI::Spi *spi;
+
+    protected:
+        esp_err_t writeByteData(const uint8_t reg, const uint8_t value);
+        int readByteData(const uint8_t reg);
+        int readWordData(const uint8_t reg);
+        esp_err_t readBlockData(const uint8_t reg, uint8_t *buf, const int length);
+
+    public:
+        esp_err_t InitSpi(CPPSPI::Spi *i_spi, const int ss);
+    }; // namespace CPPBME280
+}
